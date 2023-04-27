8452b423e63cd9bea9873f41e6df929e3cae7aac
Jovanny Carlos Gonzalez <jovannycarlosgonzalez9@gmail.com>
2023-04-27 11:54:55 -0600

    tema y estilo de la app terminados

---
+++ lib/config/theme/app_theme.dart
@@ -0,0 +1,25 @@
+
+
+import 'package:flutter/material.dart';
+
+const colorList =[
+  Colors.blue,
+  Colors.teal,
+  Colors.green,
+  Colors.red,
+  Colors.purple,
+  Colors.deepPurple,
+  Colors.orange,
+  Colors.pinkAccent,
+];
+class AppTheme{
+  final int selectedColor;
+
+  AppTheme({this.selectedColor= 0}): assert(selectedColor >= 0,' Selected color must be greater than 0'),
+                                    assert(selectedColor <= colorList.length-1,' Selected color must be less or equal than ${colorList.length}');
+
+ThemeData getTheme()=> ThemeData(
+  useMaterial3: true,
+  colorSchemeSeed: colorList[selectedColor]
+);
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:widgets_app/config/theme/app_theme.dart';
+
 
 void main() {
   runApp(const MainApp());
@@ -9,12 +11,16 @@ class MainApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
-      home: Scaffold(
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      theme: AppTheme(selectedColor: 0).getTheme(),
+      home:  Scaffold(
         body: Center(
-          child: Text('Hello World!'),
+          child: FilledButton(onPressed: () {  },
+          child: const Text('Hello World!')),
         ),
       ),
+     
     );
   }
 }
