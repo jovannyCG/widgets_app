a06bff45129c048201138fbfcd973ba1ec29e3f4
Jovanny Carlos Gonzalez <jovannycarlosgonzalez9@gmail.com>
2023-05-02 12:21:21 -0600

    pantalla de progress indicator terminada

--- lib/presentation/screens/progress/progress_screen.dart
+++ lib/presentation/screens/progress/progress_screen.dart
@@ -34,11 +34,11 @@ class _ProgressView extends StatelessWidget {
           strokeWidth: 2,
           backgroundColor: Colors.black45,
         ),
-         SizedBox(
+        SizedBox(
           height: 20,
         ),
         Text('Circular Progress indicator controlado'),
-          SizedBox(
+        SizedBox(
           height: 20,
         ),
         _ControlledProgressIndicator()
@@ -52,6 +52,31 @@ class _ControlledProgressIndicator extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return StreamBuilder(
+        stream: Stream.periodic(const Duration(milliseconds: 300), (value) {
+          return (value * 2) / 100;
+        }).takeWhile((value) => value < 100),
+        builder: (context, snapshot) {
+          final progressValue = snapshot.data ?? 0;
+          return Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 20),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children:  [
+                CircularProgressIndicator(
+                  value:  progressValue,
+                  strokeWidth: 2,
+                  backgroundColor: Colors.black45,
+                ),
+                const SizedBox(
+                  width: 20,
+                ),
+                Expanded(child: LinearProgressIndicator(
+                  value:  progressValue,
+                ))
+              ],
+            ),
+          );
+        });
   }
 }
