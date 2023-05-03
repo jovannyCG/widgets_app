53000206149b53c28c9c3c473cf2fa5f25fe0226
Jovanny Carlos Gonzalez <jovannycarlosgonzalez9@gmail.com>
2023-05-03 13:15:43 -0600

    page view con imagenes añadida

--- lib/presentation/screens/app_tutorial/app_tutorial.dart
+++ lib/presentation/screens/app_tutorial/app_tutorial.dart
@@ -1,15 +1,19 @@
 import 'package:flutter/material.dart';
+import 'package:go_router/go_router.dart';
 
 final slides = <SlideInfo>[
-  SlideInfo('buscar comida', 
-  'buscVoluptate ad ea pariatur consequat minim velit ut mollit nostrud. Fugiat proident sint adipisicing cillum enim consequat Lorem. Ad et et consequat quis commodo.', 
-  'assets/1.png'),
-  SlideInfo('Entrega rápida', 
-  'Dolore ad exercitation in irure velit cupidatat id fugiat voluptate id. Cillum cillum laboris sit ullamco veniam Lorem commodo est veniam tempor qui sint. Do nisi in reprehenderit ullamco dolore occaecat proident. Cillum consequat mollit esse labore ut id.',
-  'assets/2.png'),
-  SlideInfo('Disfruta la comida',
-  'Eu ea dolore ex sunt ea qui amet. Nostrud duis labore cillum quis pariatur id cupidatat consequat laborum. Veniam sint consequat veniam in amet voluptate enim. Magna est eiusmod veniam enim exercitation ullamco et exercitation Lorem id aliquip laboris aliqua. Minim nulla ex fugiat nulla. Incididunt esse voluptate voluptate sint ex magna sunt. Aliqua do veniam amet voluptate ut eiusmod magna et proident laboris officia.', 
-  'assets/3.png')
+  SlideInfo(
+      'buscar comida',
+      'buscVoluptate ad ea pariatur consequat minim velit ut mollit nostrud. Fugiat proident sint adipisicing cillum enim consequat Lorem. Ad et et consequat quis commodo.',
+      'assets/1.png'),
+  SlideInfo(
+      'Entrega rápida',
+      'Dolore ad exercitation in irure velit cupidatat id fugiat voluptate id. Cillum cillum laboris sit ullamco veniam Lorem commodo est veniam tempor qui sint. Do nisi in reprehenderit ullamco dolore occaecat proident. Cillum consequat mollit esse labore ut id.',
+      'assets/2.png'),
+  SlideInfo(
+      'Disfruta la comida',
+      'Eu ea dolore ex sunt ea qui amet. Nostrud duis labore cillum quis pariatur id cupidatat consequat laborum. Veniam sint consequat veniam in amet voluptate enim. Magna est eiusmod veniam enim exercitation ullamco et exercitation Lorem id aliquip laboris aliqua. Minim nulla ex fugiat nulla. Incididunt esse voluptate voluptate sint ex magna sunt. Aliqua do veniam amet voluptate ut eiusmod magna et proident laboris officia.',
+      'assets/3.png')
 ];
 
 class SlideInfo {
@@ -27,27 +31,66 @@ class AppTutorialPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        body: PageView(
-      physics: const BouncingScrollPhysics(),
-      children: 
-       slides.map((slideData) => _SlideView(
-        title: slideData.title,
-        caption: slideData.caption, 
-        imageUrl: slideData.imageUrl, 
-        )).toList()
-      
-    ));
+      backgroundColor: Colors.white,
+        body: Stack(
+          children: [
+
+            PageView(
+                physics: const BouncingScrollPhysics(),
+                children: slides
+                    .map((slideData) => _SlideView(
+                          title: slideData.title,
+                          caption: slideData.caption,
+                          imageUrl: slideData.imageUrl,
+                        ))
+                    .toList()),
+
+                    Positioned(
+                      right: 20,
+                      top: 50,
+                      child: TextButton(onPressed: ()=> context.pop(), child: const Text('salir')))
+          ],
+        ));
   }
 }
+
 class _SlideView extends StatelessWidget {
   final String title;
   final String caption;
   final String imageUrl;
 
-  const _SlideView({required this.title, required this.caption, required this.imageUrl});
+  const _SlideView(
+      {required this.title, required this.caption, required this.imageUrl});
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    final titleStyle = Theme.of(context).textTheme.titleLarge;
+    final captionStyle = Theme.of(context).textTheme.bodySmall;
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 30),
+      child: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Image(image: AssetImage(imageUrl)),
+            const SizedBox(
+              height: 20,
+            ),
+            Text(
+              title,
+              style: titleStyle,
+            ),
+            const SizedBox(
+              height: 10,
+            ),
+            Text(
+              caption,
+              style: captionStyle,
+            ),
+          ],
+        ),
+      ),
+    );
   }
 }
