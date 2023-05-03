11122423429cb5c1ba6abebfa7ca456fae6f6e04
Jovanny Carlos Gonzalez <jovannycarlosgonzalez9@gmail.com>
2023-05-03 10:06:20 -0600

    propiedades de contenedor animadas

--- lib/presentation/screens/animated/animated_screen.dart
+++ lib/presentation/screens/animated/animated_screen.dart
@@ -1,10 +1,31 @@
 import 'package:flutter/material.dart';
+import 'dart:math'show Random;
 
-
-class AnimatedPage extends StatelessWidget {
+class AnimatedPage extends StatefulWidget {
 static const String name = 'Animated_screen';
 
   const AnimatedPage({super.key});
+
+  @override
+  State<AnimatedPage> createState() => _AnimatedPageState();
+}
+
+class _AnimatedPageState extends State<AnimatedPage> {
+  double width = 50;
+  double height =50;
+  Color color = Colors.amber;
+  double boderRadius = 30;
+
+  void changeShape(){
+    final random = Random();
+    width = random.nextInt(300) + 120;
+    height = random.nextInt(300)+ 120;
+    boderRadius = random.nextInt(100)+ 20;
+    color = Color.fromRGBO(random.nextInt(255),random.nextInt(255),random.nextInt(255),1);
+    setState(() {
+      
+    });
+  }
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -13,18 +34,18 @@ static const String name = 'Animated_screen';
       ),
       body: Center(
         child: AnimatedContainer(
-          curve: Curves.easeInOutCubic,
-          width: 200,
-          height: 100,
+          curve: Curves.elasticOut,
+          width: width <=0 ? 0: width,
+          height: height <=0 ? 0: height,
           decoration: BoxDecoration(
-            color: Colors.amber,
-            borderRadius: BorderRadius.circular(20)
+            color: color,
+            borderRadius: BorderRadius.circular(boderRadius <0 ? 0:boderRadius)
           ), duration: const Duration(milliseconds: 900),
 
         ),
       ),
     floatingActionButton: FloatingActionButton(
-      onPressed: (){},
+      onPressed: changeShape,
       child: const Icon(Icons.play_arrow_rounded),
       ),
    );
