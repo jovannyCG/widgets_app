98a2a9bd6d3baf3e1fd129307f4cb7376ae5e03a
Jovanny Carlos Gonzalez <jovannycarlosgonzalez9@gmail.com>
2023-05-04 12:58:48 -0600

    indicaldor de carga de imagenes terminado

--- lib/presentation/screens/infinite_scroll/infinite_scroll.dart
+++ lib/presentation/screens/infinite_scroll/infinite_scroll.dart
@@ -1,3 +1,4 @@
+import 'package:animate_do/animate_do.dart';
 import 'package:flutter/material.dart';
 import 'package:go_router/go_router.dart';
 
@@ -25,6 +26,57 @@ class _InfiniteScrollPageState extends State<InfiniteScrollPage> {
     11,
     12,
   ];
+  final scrollController = ScrollController();
+  bool isLoading = false;
+  bool isMounted = true;
+  @override
+  void initState() {
+    super.initState();
+    scrollController.addListener(() {
+      if ((scrollController.position.pixels + 500) >=
+          scrollController.position.maxScrollExtent) {
+        loadNextPage();
+      }
+    });
+  }
+
+  @override
+  void dispose() {
+    scrollController.dispose();
+    isMounted = false;
+    super.dispose();
+  }
+
+  Future loadNextPage() async {
+    if (isLoading) return;
+    isLoading = true;
+    setState(() {});
+    await Future.delayed(const Duration(seconds: 2));
+    addFiveImages();
+    isLoading = false;
+    if (!isMounted) return;
+    setState(() {});
+  }
+
+  void addFiveImages() {
+    final lasId = imagesIds.last;
+    imagesIds.addAll([1, 2, 3, 4, 5].map((e) => lasId + e));
+  }
+
+  Future<void> onRefresh() async {
+    isLoading = true;
+    setState(() {});
+    await Future.delayed(const Duration(seconds: 3));
+    if (!isMounted) return;
+    isLoading = false;
+    final lasId = imagesIds.last;
+    imagesIds.clear();
+    imagesIds.add(lasId + 1);
+    addFiveImages();
+
+    setState(() {});
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -33,23 +85,46 @@ class _InfiniteScrollPageState extends State<InfiniteScrollPage> {
         context: context,
         removeTop: true,
         removeBottom: true,
-        child: ListView.builder(
-            physics: const BouncingScrollPhysics(),
-            itemCount: imagesIds.length,
-            itemBuilder: (context, index) {
-              return FadeInImage(
-                  fit: BoxFit.cover,
-                  width: double.infinity,
-                  height: 300,
-                  placeholder: const AssetImage('assets/jar-loading.gif'),
-                  image: NetworkImage(
-                      'https://picsum.photos/id/${imagesIds[index]}/200/300'));
-            }),
+        child: RefreshIndicator(
+          edgeOffset: 10,
+          strokeWidth: 2,
+            onRefresh: onRefresh,
+            child: _ImageList(
+                scrollController: scrollController, imagesIds: imagesIds)),
       ),
       floatingActionButton: FloatingActionButton(
-        onPressed: () => context.pop(),
-        child: const Icon(Icons.arrow_back),
-      ),
+          onPressed: () => context.pop(),
+          child: isLoading
+              ? SpinPerfect(
+                  infinite: true, child: const Icon(Icons.refresh_rounded))
+              : FadeIn(child: const Icon(Icons.arrow_back))),
     );
+  }
+}
+
+class _ImageList extends StatelessWidget {
+  const _ImageList({
+    required this.scrollController,
+    required this.imagesIds,
+  });
+
+  final ScrollController scrollController;
+  final List<int> imagesIds;
+
+  @override
+  Widget build(BuildContext context) {
+    return ListView.builder(
+        controller: scrollController,
+        physics: const BouncingScrollPhysics(),
+        itemCount: imagesIds.length,
+        itemBuilder: (context, index) {
+          return FadeInImage(
+              fit: BoxFit.cover,
+              width: double.infinity,
+              height: 300,
+              placeholder: const AssetImage('assets/jar-loading.gif'),
+              image: NetworkImage(
+                  'https://picsum.photos/id/${imagesIds[index]}/200/300'));
+        });
   }
 }
