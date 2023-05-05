e3bc8b39b0833621266e5f1ca1d8a3ac46ae06c4
Jovanny Carlos Gonzalez <jovannycarlosgonzalez9@gmail.com>
2023-05-05 10:55:39 -0600

    cambio de valores y modificadores terminado

--- lib/presentation/screens/counter/counter_screen.dart
+++ lib/presentation/screens/counter/counter_screen.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:widgets_app/presentation/providers/counter_provider.dart';
+import 'package:widgets_app/presentation/providers/theme_provider.dart';
 
 class CounterPage extends ConsumerWidget {
   static const String name = 'counter_screen';
@@ -9,8 +10,18 @@ class CounterPage extends ConsumerWidget {
   @override
   Widget build(BuildContext context, WidgetRef ref) {
     final clickCounter = ref.watch(counterProvider);
+    final isDarkModeProvider = ref.watch(isDarkmodeProvider);
     return Scaffold(
       appBar: AppBar(
+        actions: [
+          IconButton(
+              onPressed: () {
+                ref.read(isDarkmodeProvider.notifier).update((isDarkMode) => !isDarkMode);
+              },
+              icon: isDarkModeProvider
+                  ? const Icon(Icons.dark_mode_outlined)
+                  : const Icon(Icons.light_mode_outlined),)
+        ],
         title: const Text('Counter Screen'),
       ),
       body: Center(
@@ -20,7 +31,9 @@ class CounterPage extends ConsumerWidget {
         ),
       ),
       floatingActionButton: FloatingActionButton(
-        onPressed: () {},
+        onPressed: () {
+          ref.read(counterProvider.notifier).state++;
+        },
         child: const Icon(Icons.add),
       ),
     );
